0ccdddf5171bbdbb97f0a7230cc3f156d482a835
mergirl <nilsonferreira_@hotmail.com>
2019-08-25 22:39:46 -0300

    Criação da main e conclusão de métodos

--- src/Rubro_Negra/Main.java
+++ src/Rubro_Negra/Main.java
@@ -2,9 +2,18 @@ package Rubro_Negra;
 
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) throws VoidColor {
+		PilhaColor p = new PilhaColor();
+		
+		p.pushb(1);
+		p.pushr(2);
+		p.pushr(3);
+		p.pushb(4);
+		p.pushr(5);
+		
+		System.out.println(p.blacktop());
+		System.out.println(p.redtop());
+		System.out.println(p.ToString());
 	}
 
 }
