3dbea800ef23afb44fd0c5adaa076b48c42b5170
mergirl <nilsonferreira_@hotmail.com>
2019-09-02 00:25:01 -0300

    implementando o first

--- src/Fila/Main.java
+++ src/Fila/Main.java
@@ -17,24 +17,9 @@ public class Main {
 		System.out.println("PAINEL DE OPERAÇÕES\n");
 	
 		while(!fila.isEmpty()){
-			try {
-				Object x = fila.retirar();
-				System.out.println("Removido o Objeto " + x + " da fila");
-				System.out.println("Tamanho atual da fila: " + fila.size() + "\n");
-				} 
-			
-			catch (Exception e){
-				// TODO: handle exception
-				System.out.println(e.getMessage());
-			}
-		}
-		
-		try {
-			Object y = fila.retirar();
-		} catch (Exception e) {
-			// TODO: handle exception
-			System.out.println(e.getMessage());
-		}
+			System.out.println("Elemento removido: " + fila.first());
+			fila.retirar();
+		} 
+	}		
 
-	}
 }
