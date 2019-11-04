5c35767f4651d5c1bb5b40bc031fe7266d2c4078
Josenilson Ferreira de Almeida <ferreira.a@academico.ifrn.edu.br>
2019-11-04 14:02:12 -0300

    comentarios mil

--- src/heap/src/heap/Heap.java
+++ src/heap/src/heap/Heap.java
@@ -37,14 +37,18 @@ public class Heap implements InterfaceHeap {
 
 	public void downheap() {
 		int painho = 1, esq, dir, k = 0;
+		
 		while (true) {
 			esq = 2 * painho;
 			dir = 2 * painho + 1;
+			//verifica qual lado é o filho folha
 			if (esq > size && dir > size) {
 				break;
+			//verifica também se é menor que os filhos
 			} else if ((((Integer) this.array[painho]) < ((Integer) this.array[esq])
 					&& (Integer) this.array[painho] < (Integer) this.array[dir])) {
 				break;
+			//se nenhuma condição for verdadeira, verifica qual dos filhos é o menor e armazena na variável k
 			} else {
 				if ((Integer) this.array[dir] < (Integer) this.array[esq]) {
 					k = dir;
@@ -52,16 +56,20 @@ public class Heap implements InterfaceHeap {
 					k = esq;
 				}
 			}
+			//realiza a troca do pai maior com o filho menor
 			int auxpai = (Integer) this.array[k];
 			this.array[k] = this.array[painho];
 			this.array[painho] = auxpai;
+			//atualiza a variavel pai para continuar a verificação
 			painho = k;
 		}
 	}
 
 	public void upheap() {
 		int aux = size;
+		//verifica se o nó é raiz ou se o filho é maior que o pai
 		while (!(aux == 1 || (Integer) this.array[aux] > (Integer) this.array[aux / 2])) {
+			//se passar no while, significa que o nó a ser trocado não é raiz e também é menor que o pai.
 			int auxpai = (Integer) this.array[aux / 2];
 			this.array[aux / 2] = this.array[aux];
 			this.array[aux] = auxpai;
@@ -69,13 +77,11 @@ public class Heap implements InterfaceHeap {
 		}
 	}
 
-	@Override
 	public void insert(Object o) {
 		this.array[++size] = o;
 		upheap();
 	}
 
-	@Override
 	public void display() {
 		for(int i = 1; i <= size; i++){
 			if(i == size) System.out.print(this.array[i] + "\n");
